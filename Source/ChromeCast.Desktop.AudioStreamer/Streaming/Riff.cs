b9808c2e2848531b899860cc1d38cacc46c131ff
Lukas <karim.luccin@gmail.com>
2018-12-30 22:57:15 -0800

    Riff

--- Source/ChromeCast.Desktop.AudioStreamer/Streaming/Riff.cs
+++ Source/ChromeCast.Desktop.AudioStreamer/Streaming/Riff.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using NAudio.Wave;
 using ChromeCast.Desktop.AudioStreamer.Streaming.Interfaces;
+using System;
 
 namespace ChromeCast.Desktop.AudioStreamer.Streaming
 {
@@ -12,26 +13,61 @@ namespace ChromeCast.Desktop.AudioStreamer.Streaming
         /// </summary>
         public byte[] GetRiffHeader(WaveFormat format)
         {
-            var dataSize = (uint)0;
-            uint chunkSize = dataSize;
-            uint factChunkSize = 4;
-            uint numberOfSamples = (uint)((dataSize * 8) / format.BitsPerSample / format.Channels);
-
             var riffHeaderStream = new MemoryStream();
-            var writer = new BinaryWriter(riffHeaderStream, Encoding.UTF8);
-
-            writer.Write(Encoding.UTF8.GetBytes("RIFF"));
-            writer.Write(chunkSize);
-            writer.Write(Encoding.UTF8.GetBytes("WAVE"));
-            writer.Write(Encoding.UTF8.GetBytes("fmt "));
-            format.Serialize(writer);
-            writer.Write(Encoding.UTF8.GetBytes("fact"));
-            writer.Write(factChunkSize);
-            writer.Write(numberOfSamples);
-            writer.Write(Encoding.UTF8.GetBytes("data"));
-            writer.Write(dataSize);
+            WriteWavHeader(riffHeaderStream, format.Encoding == WaveFormatEncoding.IeeeFloat, (ushort)format.Channels, (ushort)format.BitsPerSample, format.SampleRate, 0);
 
             return riffHeaderStream.ToArray();
+        }
+
+        private static void WriteWavHeader(MemoryStream stream, bool isFloatingPoint, ushort channelCount, ushort bitDepth, int sampleRate, int totalSampleCount)
+        {
+            stream.Position = 0;
+
+            // RIFF header.
+            // Chunk ID.
+            stream.Write(Encoding.ASCII.GetBytes("RIFF"), 0, 4);
+
+            // Chunk size.
+            stream.Write(BitConverter.GetBytes(((bitDepth / 8) * totalSampleCount) + 36), 0, 4);
+
+            // Format.
+            stream.Write(Encoding.ASCII.GetBytes("WAVE"), 0, 4);
+
+
+
+            // Sub-chunk 1.
+            // Sub-chunk 1 ID.
+            stream.Write(Encoding.ASCII.GetBytes("fmt "), 0, 4);
+
+            // Sub-chunk 1 size.
+            stream.Write(BitConverter.GetBytes(16), 0, 4);
+
+            // Audio format (floating point (3) or PCM (1)). Any other format indicates compression.
+            stream.Write(BitConverter.GetBytes((ushort)(isFloatingPoint ? 3 : 1)), 0, 2);
+
+            // Channels.
+            stream.Write(BitConverter.GetBytes(channelCount), 0, 2);
+
+            // Sample rate.
+            stream.Write(BitConverter.GetBytes(sampleRate), 0, 4);
+
+            // Bytes rate.
+            stream.Write(BitConverter.GetBytes(sampleRate * channelCount * (bitDepth / 8)), 0, 4);
+
+            // Block align.
+            stream.Write(BitConverter.GetBytes((ushort)channelCount * (bitDepth / 8)), 0, 2);
+
+            // Bits per sample.
+            stream.Write(BitConverter.GetBytes(bitDepth), 0, 2);
+
+
+
+            // Sub-chunk 2.
+            // Sub-chunk 2 ID.
+            stream.Write(Encoding.ASCII.GetBytes("data"), 0, 4);
+
+            // Sub-chunk 2 size.
+            stream.Write(BitConverter.GetBytes((bitDepth / 8) * totalSampleCount), 0, 4);
         }
     }
 }
